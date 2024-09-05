c31e2358913d5741c8783ba7753edf627db4d808
khalid <dev.khalidhossain@gmail.com>
2024-09-05 18:14:07 +0600

    refactor:(Footer) add important links and navs

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import About from "components/About/About";
 import Contact from "components/Contact/Contact";
-import Signature from "components/Contact/Signature";
 import Experience from "components/Experience/Experience";
 import HeroSection from "components/Hero/HeroSection";
 import Works from "components/Works/Works";
@@ -9,11 +8,11 @@ export default function Home() {
   return (
     <main className=" relative overflow-hidden" id="MainTag">
       <HeroSection />
+
       <Experience />
       <Works />
       <About />
       <Contact />
-      <Signature />
     </main>
   );
 }
