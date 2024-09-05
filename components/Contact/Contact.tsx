fc175d9d04627f1004b4771897a79f0c394445e4
khalid <dev.khalidhossain@gmail.com>
2024-09-05 18:14:07 +0600

    refactor:(Footer) add important links and navs

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -1,11 +1,14 @@
+"use client";
 import Link from "next/link";
+import "./Contact.module.css";
+import Signature from "./Signature";
 export default function Contact() {
   return (
     <footer
       id="contact"
       className="bg-slate-100 dark:bg-slate-900 text-slate-950 dark:text-slate-200 "
     >
-      <main className="flex justify-around flex-col mx-auto min-h-screen p-4 sm:w-11/12 md:w-4/6 xl:w-3/6 ">
+      <main className="flex justify-between flex-col mx-auto min-h-screen p-4 sm:w-11/12 md:w-4/6 xl:w-3/6 text-base">
         <section>
           <h1 className="h1 my-16 text-center text-4xl font-black  md:text-6xl lg:text-8xl ">
             Let&apos;s Connect
@@ -17,8 +20,8 @@ export default function Contact() {
             on exciting projects, or simply exchanging ideas. Feel free to reach
             out via{" "}
             <Link
-              href="@mail:dev.khalidhossain@gmail.com"
-              className="text-indigo-500 underline"
+              href="mailto:dev.khalidhossain@gmail.com"
+              className="text-indigo-500"
             >
               email
             </Link>{" "}
@@ -32,28 +35,64 @@ export default function Contact() {
             . Let&apos;s create something extraordinary together!
           </article>
         </section>
+        {/* Connect with me ================= */}
         <section>
-          <h1 className="h2 text-2xl font-semibold my-4">Connect me on.</h1>
-          <ul className="flex flex-col gap-4">
-            <li>
-              <Link href="#">URL</Link>
-            </li>
-            <li>
-              <Link href="#">URL</Link>
-            </li>
-            <li>
-              <Link href="#">URL</Link>
-            </li>
-            <li>
-              <Link href="#">URL</Link>
-            </li>
-            <li>
-              <Link href="#">URL</Link>
-            </li>
-          </ul>
+          <h1 className="h2 text-2xl font-semibold my-4  border-b">
+            Connect me Via.
+          </h1>
+          <div className="flex justify-between items-center flex-wrap">
+            <ul className="flex flex-col gap-4 social-links">
+              <li>
+                <Link
+                  href="https://www.facebook.com/badhon252"
+                  target="_blank"
+                  className="text-indigo-500"
+                >
+                  @facebook/badhon252
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="https://www.linkedin.com/in/khalidhossainbadhon/"
+                  target="_blank"
+                  className="text-indigo-500"
+                >
+                  @linkedin/khalidhossainbadhon
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="https://github.com/badhon252/"
+                  target="_blank"
+                  className="text-indigo-500"
+                >
+                  @github/badhon252
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="https://t.me/khalidhossainbadhon"
+                  target="_blank"
+                  className="text-indigo-500"
+                >
+                  @t.me/khalidhossain
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="https://twitter.com/badhon252"
+                  target="_blank"
+                  className="text-indigo-500"
+                >
+                  @twitter.com/badhon252
+                </Link>
+              </li>
+            </ul>
+            <Signature />
+          </div>
         </section>
 
-        <p className="text-center">
+        <p className="text-center opacity-60 hover:opacity-80">
           All rights reserved by{" "}
           <Link
             href="https://github.com/badhon252"
@@ -63,6 +102,12 @@ export default function Contact() {
             @badhon252
           </Link>
         </p>
+        <button
+          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+          className="fixed bottom-4 right-4 p-2 bg-indigo-500 text-white rounded-full"
+        >
+          ↑ Top
+        </button>
       </main>
     </footer>
   );
--- components/Contact/Signature.tsx
+++ components/Contact/Signature.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
+import "./Contact.module.css";
 
 export default function Signature() {
   return (
-    <div className="contact-mail text-indigo-400 bg-slate-100 dark:bg-slate-950 text-xl text-center py-12">
-      <Link href="#" className="email">
-        dev.khalidhossain@gmail.com <span className="">{"-->"}</span>
+    <div className="my-8 contact-mail px-4 py-2 bg-indigo-500 text-white rounded text-center ">
+      <Link href="mailto:dev.khalidhossain@gmail.com" className="email">
+        dev.khalidhossain@gmail.com{" "}
+        <span className="email-indicator">{" --> "}</span>
       </Link>
     </div>
   );
