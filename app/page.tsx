57503cc2b7c3c3b0608225536861609881db9109
khalid <dev.khalidhossain@gmail.com>
2024-09-10 01:43:53 +0600

    refactor: responsive issue and UI color fixed

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ export default function Home() {
   return (
     <main className="  overflow-hidden" id="MainTag">
       <HeroSection />
-
       <Experience />
       <Works />
       <About />
--- components/Experience/JobDescription.tsx
+++ components/Experience/JobDescription.tsx
@@ -1,9 +1,8 @@
 "use client";
-import { motion } from "framer-motion"; // Importing framer-motion for animations
+import Link from "next/link";
 import { useState } from "react";
 import "styles/style.css";
 
-// Data structure to hold experience details
 interface ExperienceDetails {
   companyName: string;
   jobTitle: string;
@@ -11,7 +10,6 @@ interface ExperienceDetails {
   responsibilities: string[];
 }
 
-// Array of job experiences for each company
 const experienceDetails: ExperienceDetails[] = [
   {
     companyName: "Babylon Resource ltd",
@@ -38,50 +36,43 @@ const experienceDetails: ExperienceDetails[] = [
 ];
 
 export default function Experience() {
-  // State to keep track of which company's job description is currently open
-  const [selectedCompany, setSelectedCompany] = useState<string>(
-    experienceDetails[0].companyName, // Default to the first company being selected/open
-  );
+  const [selectedCompany, setSelectedCompany] = useState<string | null>(null);
 
-  // Function to handle company clicks and expand the corresponding job description
   const handleCompanyClick = (company: string) => {
-    setSelectedCompany(company);
+    if (selectedCompany === company) {
+      setSelectedCompany(null); // Collapse if clicked again
+    } else {
+      setSelectedCompany(company); // Show selected company's job description
+    }
   };
 
   return (
-    <main className="md:min-h-screen mx-auto md:flex md:items-center bg-gradient-to-t from-slate-100 to-cyan-200 dark:text-gray-100 dark:bg-gradient-to-t dark:from-slate-800 dark:to-gray-950 text-slate-700 py-12">
-      {/* Main container for the experience section */}
+    <main className="mx-auto bg-gradient-to-t from-slate-100 to-cyan-200 dark:text-gray-100 dark:bg-gradient-to-t dark:from-slate-800 dark:to-gray-950 text-slate-700 py-12">
       <section
         id="experience"
         className="sm:w-12/12 mx-auto flex flex-col justify-center md:w-4/6 xl:w-3/6 container text-slate-700 dark:text-gray-200"
       >
-        {/* Section title */}
         <h1 className="h1 text-bold mb-12 text-center text-4xl font-black md:text-6xl lg:text-8xl">
           Experiences
         </h1>
 
-        {/* Experience layout */}
         <div className="experience flex flex-wrap">
-          {/* Sidebar: List of companies */}
+          {/* Company List */}
           <div className="md:basis-1/3 text-lg font-semibold">
             <ul className="company mx-8">
               {experienceDetails.map((experience) => (
                 <li
-                  key={experience.companyName} // Unique key for each list item
-                  className={` rounded-lg py-2 md:px-4 my-4 text-base cursor-pointer ${
-                    selectedCompany === experience.companyName
-                      ? "bg-teal-300 dark:text-slate-800" // Highlight the selected company
-                      : ""
-                  }`}
-                  onClick={() => handleCompanyClick(experience.companyName)} // Handle click to show job description
+                  key={experience.companyName}
+                  className="brlbd rounded-lg py-2 my-4 text-base cursor-pointer"
+                  onClick={() => handleCompanyClick(experience.companyName)}
                 >
-                  <>{experience.companyName}</>
+                  <Link href="#">{experience.companyName}</Link>
                 </li>
               ))}
             </ul>
           </div>
 
-          {/* Main Content: Job Description */}
+          {/* Job Description */}
           <div
             id="jobDescription"
             className="md:basis-2/3 border-l-2 border-slate-100 px-8"
@@ -89,32 +80,21 @@ export default function Experience() {
             {experienceDetails.map(
               (experience) =>
                 selectedCompany === experience.companyName && (
-                  <motion.div
-                    key={experience.companyName}
-                    initial={{ opacity: 0, y: -10 }} // Animation starts from here (hidden)
-                    animate={{ opacity: 1, y: 0 }} // Animates to visible and moves into place
-                    transition={{ duration: 0.5 }} // Duration of the animation
-                  >
-                    {/* Job title and time period */}
+                  <div key={experience.companyName}>
                     <h2 className="text-xl">{experience.jobTitle}</h2>
                     <p className="text-sm text-indigo-500">
                       {experience.timePeriod}
                     </p>
-
-                    {/* Responsibilities list */}
                     <ul className="my-4 text-slate-700 dark:text-slate-300">
                       {experience.responsibilities.map(
                         (responsibility, index) => (
-                          <li
-                            key={index}
-                            className="experience-details my-4 text-slate-600 dark:text-slate-400"
-                          >
+                          <li key={index} className="experience-details my-4">
                             <article>{responsibility}</article>
                           </li>
                         ),
                       )}
                     </ul>
-                  </motion.div>
+                  </div>
                 ),
             )}
           </div>
